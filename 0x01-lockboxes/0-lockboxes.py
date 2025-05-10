3bb60b926b8ad705515fd4e5b83f49cae1ed5f84
Joshua Baka <fatow.jay@gmail.com>
2025-05-10 23:47:05 +0000

    PEP8 issues fixed

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,8 +9,8 @@ def canUnlockAll(boxes):
     Determines if all the boxes can be opened
 
     Args:
-        boxes (list of lists): A list of boxes where each box may contain keys to other boxes
-                              Box i contains a key to box j if boxes[i] contains j
+        boxes (list of lists): A list of boxes where each box may contain
+                              keys to other boxes
 
     Returns:
         bool: True if all boxes can be opened, False otherwise
@@ -19,19 +19,19 @@ def canUnlockAll(boxes):
     # Keep track of which boxes we can open
     unlocked = [False] * n
     unlocked[0] = True  # First box is already unlocked
-    
+
     # Stack to keep track of keys to process
     keys_to_check = [0]  # Start with the first box
-    
+
     while keys_to_check:
         current_box = keys_to_check.pop()
-        
+
         # Check each key in the current box
         for key in boxes[current_box]:
-            # If the key opens a box we haven't unlocked yet and is within range
+            # If the key opens a box we haven't unlocked yet
             if key < n and not unlocked[key]:
                 unlocked[key] = True
                 keys_to_check.append(key)
-    
+
     # Check if all boxes are unlocked
     return all(unlocked)
